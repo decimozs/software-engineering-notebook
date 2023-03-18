04c7498ca7d7546095774d49fc301fc848fc87f9
DecimoDevs <marlonadiguemartin548@gmail.com>
2023-03-18 20:32:02 +0800

    hackerrank!

--- Arsenals/HackerRank/JavaIfElse.java
+++ Arsenals/HackerRank/JavaIfElse.java
@@ -19,14 +19,18 @@ public class JavaIfElse {
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
 
-        if(N % 2 != 0){
+        if (N %2 == 0) {
+            if (N >= 2 && N <=5) {
+                System.out.println("Not Weird");
+            }
+            else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            }
+            else {
+                System.out.println("Not Weird");
+            }
+        } else {
             System.out.println("Weird");
-        }else if(N % 2 == 0 && N <= 2 && N >= 5){
-            System.out.println("Not Weird");
-        }else if(N % 2 == 0 && N <= 6 && N >= 20){
-            System.out.println("Weird");
-        }else{
-            System.out.println("Not Weird");
         }
         
         bufferedReader.close();
